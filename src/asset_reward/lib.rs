12a7478695b52f37941b78e84a02e592fd62c1a5
Leandro <leapalazzolo@gmail.com>
2023-10-26 12:30:22 -0300

    fix: needless borrow

--- src/asset_reward/lib.rs
+++ src/asset_reward/lib.rs
@@ -257,7 +257,7 @@ mod asset_reward {
         /// A helper function to recover the signer from a ECDSA signature from a given message.
         pub fn recover_signer(signature: &SignatureValue, message: &HashValue) -> Result<[u8; 33]> {
             let mut output: [u8; 33] = [0; 33];
-            match ink::env::ecdsa_recover(&signature, &message, &mut output) {
+            match ink::env::ecdsa_recover(signature, message, &mut output) {
                 Ok(_) => Ok(output),
                 Err(_) => Err(Error::InvalidSigner),
             }
