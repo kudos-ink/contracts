7af0ce9c30111efb7a672c08eabc5ab56178e3e4
Igor Papandinas <igor.papandinas@posteo.net>
2023-10-27 16:57:30 +0200

    feat: Ownable implementation added

--- src/asset_reward/lib.rs
+++ src/asset_reward/lib.rs
@@ -1,31 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[ink::contract]
-mod asset_reward {
+#[openbrush::implementation(Ownable)]
+#[openbrush::contract]
+pub mod asset_reward {
+    use openbrush::{contracts::traits::ownable::OwnableError, modifiers, traits::Storage};
+
     use ink::env::hash::{HashOutput, Sha2x256};
     use ink::storage::Mapping;
 
     /// Type alias for hashes.
     pub type HashValue = [u8; 32];
-    /// Type alias for ECDSA signatures.
-    pub type SignatureValue = [u8; 65];
-    /// Type alias for the contract's `Result` type.
-    pub type Result<T> = core::result::Result<T, Error>;
-    /// Type alias for the workflow run message.
-    pub type WorkflowRunMessage = (HashValue, u64);
-
-    /// A Workflow is represented by:
-    /// - the public address of the account transferring the reward.
-    /// - the hash of the workflow file.
-    #[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Decode, scale::Encode)]
-    #[cfg_attr(
-        feature = "std",
-        derive(ink::storage::traits::StorageLayout, scale_info::TypeInfo)
-    )]
-    pub struct Workflow {
-        account: AccountId,
-        hash: HashValue,
-    }
 
     /// A Contribution is represented by:
     /// - a unique id.
@@ -42,18 +26,16 @@ mod asset_reward {
     }
 
     #[ink(storage)]
+    #[derive(Default, Storage)]
     pub struct AssetReward {
-        // The registered workflow.
-        workflow: Workflow,
+        #[storage_field]
+        ownable: ownable::Data,
 
-        // The public signer account that signs the hash of the `WorkflowRunMessage`, a tuple composed of the workflow file hash and a specific workflow run id.
-        signer: AccountId,
+        // The registered workflow.
+        workflow: HashValue,
 
         // The contribution reward amount
         reward: Balance,
-
-        // The used workflow run ids database.
-        used_run_ids: Mapping<u64, bool>,
 
         // The approved contribution ids database.
         contributions: Mapping<u64, Contribution>,
@@ -66,6 +48,7 @@ mod asset_reward {
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
     pub enum Error {
+        OwnableError(OwnableError),
         /// An aspiring contributor identity is already registered in the DB.
         IdentityAlreadyRegistered,
         /// Contribution is already approved in the DB.
@@ -88,51 +71,23 @@ mod asset_reward {
         InvalidSigner,
     }
 
+    impl From<OwnableError> for Error {
+        fn from(error: OwnableError) -> Self {
+            Error::OwnableError(error)
+        }
+    }
+
     impl AssetReward {
         /// Constructor that initializes an asset reward for a given workflow
         #[ink(constructor)]
-        pub fn new(workflow: Workflow, reward: Balance, signer: AccountId) -> Self {
+        pub fn new(workflow: HashValue, reward: Balance) -> Self {
+            let mut instance = Self::default();
+            let caller = instance.env().caller();
+            ownable::Internal::_init_with_owner(&mut instance, caller);
             Self {
                 workflow,
                 reward,
-                signer,
-                used_run_ids: Mapping::default(),
-                contributions: Mapping::default(),
-                identities: Mapping::default(),
-            }
-        }
-
-        /// Verify that the correct workflow run is executed before approving a contribution.
-        /// To do so:
-        /// - it reconstructs a message using the stored hash of the workflow file with a specific workflow run id from arguments
-        /// - recovers the signer public address using the generated message and the signature from arguments
-        /// - ensure that the public address recovered is the trusted signer address in storage
-        /// Reward contracts extend this method to the implementation of their respective reward mechanisms.
-        ///
-        /// Constraint(s):
-        /// 1. The caller has to be the workflow `owner`.
-        /// 2. The workflow `run_id` must not have been used previously.
-        /// 3. The signature has to be valid.
-        #[ink(message)]
-        pub fn verify_workflow_run(
-            &mut self,
-            run_id: u64,
-            signature: SignatureValue,
-        ) -> Result<bool> {
-            if self.env().caller() != self.workflow.account {
-                return Err(Error::CallerIsNotWorkflowOwner);
-            }
-
-            if self.run_id_is_known(run_id) {
-                return Err(Error::RunIdAlreadyUsed);
-            }
-
-            let message: WorkflowRunMessage = (self.workflow.hash, run_id);
-            let message_hash = AssetReward::hash_workflow_run(&message);
-            match AssetReward::recover_signer(&signature, &message_hash) {
-                // TODO: Ok(recovered_signer) => Ok(recovered_signer == self.signer),
-                Ok(_) => Ok(true),
-                Err(_) => Err(Error::InvalidSigner),
+                ..instance
             }
         }
 
@@ -143,7 +98,11 @@ mod asset_reward {
         ///
         /// A `Registered` event is emitted.
         #[ink(message)]
-        pub fn register_identity(&mut self, account: AccountId, identity: HashValue) -> Result<()> {
+        pub fn register_identity(
+            &mut self,
+            account: AccountId,
+            identity: HashValue,
+        ) -> Result<(), Error> {
             if self.identity_is_known(identity) {
                 return Err(Error::IdentityAlreadyRegistered);
             }
@@ -155,21 +114,17 @@ mod asset_reward {
         /// Approve contribution. This is triggered by a workflow run.
         ///
         /// Constraint(s):
-        /// 1. The `run_id` & `signature` must come from a verified workflow run.
-        /// 2. The `contribution_id` should not already be approved.
-        /// 3. The `contributor_identity` must be registered.
+        /// 1. The `contribution_id` should not already be approved.
+        /// 2. The `contributor_identity` must be registered.
         ///
         /// An `Approval` event is emitted.
         #[ink(message)]
+        #[modifiers(only_owner)]
         pub fn approve(
             &mut self,
             contribution_id: u64,
             contributor_identity: HashValue,
-            run_id: u64,
-            signature: SignatureValue,
-        ) -> Result<()> {
-            let _ = self.verify_workflow_run(run_id, signature);
-
+        ) -> Result<(), Error> {
             if self.contribution_is_known(contribution_id) {
                 return Err(Error::ContributionAlreadyApproved);
             }
@@ -197,7 +152,7 @@ mod asset_reward {
         ///
         /// A `Claim` event is emitted.
         #[ink(message)]
-        pub fn claim(&self, contribution_id: u64) -> Result<bool> {
+        pub fn claim(&self, contribution_id: u64) -> Result<bool, Error> {
             if !self.contribution_is_known(contribution_id) {
                 return Err(Error::UnknownContribution);
             }
@@ -234,33 +189,11 @@ mod asset_reward {
             self.identities.get(identity).is_some()
         }
 
-        /// A helper function to detect whether a `run_id` has been already used
-        pub fn run_id_is_known(&self, run_id: u64) -> bool {
-            self.used_run_ids.get(run_id).is_some()
-        }
-
         /// A helper function to hash bytes (e.g. identities or workflow file sha)
         pub fn hash(input: &[u8]) -> HashValue {
             let mut hash_value = <Sha2x256 as HashOutput>::Type::default();
             ink::env::hash_bytes::<Sha2x256>(input, &mut hash_value);
             hash_value
-        }
-
-        /// A helper function to hash a workflow run message:
-        /// A tuple composed of the workflow file sha and the workflow run id.
-        pub fn hash_workflow_run(input: &WorkflowRunMessage) -> HashValue {
-            let mut hash_value = <Sha2x256 as HashOutput>::Type::default();
-            ink::env::hash_encoded::<Sha2x256, _>(&input, &mut hash_value);
-            hash_value
-        }
-
-        /// A helper function to recover the signer from a ECDSA signature from a given message.
-        pub fn recover_signer(signature: &SignatureValue, message: &HashValue) -> Result<[u8; 33]> {
-            let mut output: [u8; 33] = [0; 33];
-            match ink::env::ecdsa_recover(signature, message, &mut output) {
-                Ok(_) => Ok(output),
-                Err(_) => Err(Error::InvalidSigner),
-            }
         }
     }
 
@@ -294,10 +227,7 @@ mod asset_reward {
                 Ok(())
             );
             set_next_caller(accounts.alice);
-            assert_eq!(
-                contract.approve(contribution_id, identity, 0, [0; 65]),
-                Ok(())
-            );
+            assert_eq!(contract.approve(contribution_id, identity), Ok(()));
             let bob_initial_balance = get_balance(accounts.bob);
             set_next_caller(accounts.bob);
             assert_eq!(contract.claim(contribution_id), Ok(true));
@@ -335,14 +265,7 @@ mod asset_reward {
             let accounts = default_accounts();
             set_next_caller(accounts.alice);
             set_balance(contract_id(), initial_balance);
-            AssetReward::new(
-                Workflow {
-                    account: default_accounts().alice,
-                    hash: [0; 32],
-                },
-                reward,
-                accounts.frank,
-            )
+            AssetReward::new([0; 32], reward)
         }
     }
 }
